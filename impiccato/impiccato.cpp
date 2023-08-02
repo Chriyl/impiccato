28957114578cca555ce2c5b0d9c0c3aa3fdc6ba0
chris <chris@CHRISTIAN-PC>
2023-08-02 14:52:42 +0200

    Aggiungere i file di progetto.

---
+++ impiccato/impiccato.cpp
@@ -0,0 +1,16 @@
+// impiccato.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+}
+
+
