6f55cc28cf1db615b9ccf99f69a86c1cad815e3e
Christian Montanaro <christianm0602@outlook.it>
2023-08-02 18:41:22 +0200

    add randomword() e cipherword

--- impiccato/impiccato.cpp
+++ impiccato/impiccato.cpp
@@ -1,16 +1,53 @@
-// impiccato.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
 //
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
+#include <random>
 
 using namespace std;
 
+string randomWord(vector<string>, int);
+
+
 int main()
 {
+    string parola;
+    string plainWord;
+    ifstream file("parole.txt");
+
+    vector<string>* parole = new vector<string>;
     
+
+    
+    
+    for (string i;  getline(file, parola);) {
+        parole->push_back(parola);
+    }
+
+
+    int lenFile = parole->size();
+    
+    plainWord = randomWord(*parole, lenFile);
+    delete parole;
+
+    int lenWord = plainWord.size();
+
+    
+
+    string cipherWord(lenWord , '#');
+
+     return 0;
 }
 
+
+string randomWord(vector<string> parole, int size) {
+    uniform_int_distribution<> distribuzione(0, size - 1);
+    random_device rd;
+    mt19937 gen(rd());
+    int random = distribuzione(gen);
+
+    return parole[random];
+}
 
